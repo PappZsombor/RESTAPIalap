2dba5c761b119b971b1b50489e630a49fe70aadd
papp.zsombor <papp.zsombor@students.jedlik.eu>
2024-12-05 09:54:16 +0100

    test

--- __tests__/unit/restaurant.controller.test.js
+++ __tests__/unit/restaurant.controller.test.js
@@ -1,12 +1,15 @@
-const restaurantModel = require('../../models/restaurant');
+const restaurantModel = require('../../models/restaurant.model');
 const httpMocks = require('node-mocks-http');
 const newRestaurant = require('../mock-data/new-restaurant.json');
-const restaurantController = require('../../controllers/restaurant.controller'); 
+const restaurantController = require('../../controllers/restaurant.controller');
 const allRestaurants = require("../mock-data/all-restaurant.json")
 
 restaurantModel.prototype.save = jest.fn();
 restaurantModel.create = jest.fn();
 restaurantModel.find = jest.fn();
+restaurantModel.findById = jest.fn();
+restaurantModel.findByIdAndUpdate = jest.fn();
+restaurantModel.findByIdAndDelete = jest.fn();
 
 let req, res, next;
 
@@ -53,21 +56,78 @@ describe('RestaurantController.createRestaurant', () => {
     });
 });
 
+
+
 describe('restaurantController.getRestaurant', () => {
-    it("should have a getRestaurants function", () => {
+    it("should have a getRestaurant function", () => {
         expect(typeof restaurantController.getRestaurant).toBe("function");
     });
 
-    if("should call restaurantModel.find({})", async () => {
+    it("should call restaurantModel.find({})", async () => {
         await restaurantController.getRestaurant(res, req, next);
         expect(restaurantModel.find).toHaveBeenCalledWith({});
     });
 
-    it("should handle errors in getRestaurants", async () => {
-        const errorMessage = {message: "Error finding"}
-        const rejectedPromise = Promise.reject(errorMessage)
-        restaurantModel.find.mockReturnValue(rejectedPromise)
-        await restaurantController.getRestaurant(req, res, next)
-        expect(next).toBeCalledWith(errorMessage)
+    it("should return response with status 200 and all restaurants", async () => {
+        restaurantModel.find.mockReturnValue(allRestaurants);
+        await restaurantController.getRestaurant(req, res, next);
+        expect(res.statusCode).toBe(200);
+        expect(res._isEndCalled).toBeTruthy();
+        expect(res._getJSONData()).toStrictEqual(allRestaurants);
+    })
+
+    // it("should handle errors in getRestaurants", async () => {
+    //     const errorMessage = {message: "Error finding"};
+    //     const rejectedPromise = Promise.reject(errorMessage);
+    //     restaurantModel.find.mockReturnValue(rejectedPromise),
+    //     await restaurantController.getRestaurant(req, res, next);
+    //     expect(next).toBeCalledWith(errorMessage);
+    // })
+});
+
+
+describe('restaurantController.getRestaurantById', () => {
+    it("should have a getRestaurantById function", () => {
+        expect(typeof restaurantController.getRestaurantById).toBe("function");
+    });
+
+    it("should call restaurantModel.findById({})", async () => {
+        await restaurantController.getRestaurantById(res, req, next);
+        expect(restaurantModel.findById).toHaveBeenCalled;
+    });
+    it("should call restaurantModel.findById with route parameters", async () => {
+        req.params.id = "66ffb1f3e5e0f4b4efbddd59";
+        await restaurantController.getRestaurantById(res, req, next);
+        expect(restaurantModel.findById).toBeCalledWith("66ffb1f3e5e0f4b4efbddd59");
+    });
+});
+
+describe('restaurantController.updateRestaurant', () => {
+    it("should have an updateRestaurant function", () => {
+        expect(typeof restaurantController.updateRestaurant).toBe("function");
+    });
+    it("should call restaurantModel.findByIdAndUpdate({})", async () => {
+        await restaurantController.getRestaurant(res, req, next);
+        expect(restaurantModel.findByIdAndUpdate).toHaveBeenCalled;
+    });
+
+    it("should find and update by id", async () => {
+        req.params.id = restaurantId;
+        req.body = newRestaurant;
+        await restaurantController.updateToDo(req, res, next);
+        expect(restaurantModel.findByIdAndUpdate).toBeCalledWith(restaurantId, newRestaurant, {
+            new: true,
+            useFindAndModify: false,
+        })
+    })
+});
+
+describe('restaurantController.deleteRestaurant', () => {
+    it("should have a deleteRestaurant function", () => {
+        expect(typeof restaurantController.deleteRestaurant).toBe("function");
+    });
+    it("should call restaurantModel.findByIdAndDelete({})", async () => {
+        await restaurantController.getRestaurant(res, req, next);
+        expect(restaurantModel.findByIdAndDelete).toHaveBeenCalled;
     });
 });
--- controllers/restaurant.controller.js
+++ controllers/restaurant.controller.js
@@ -1,4 +1,5 @@
-const restaurantModel = require('../models/restaurant');
+const { error } = require('console');
+const restaurantModel = require('../models/restaurant.model');
 
 exports.createRestaurant = async (req, res, next) => {
     const data = new restaurantModel(req.body);
@@ -14,8 +15,39 @@ exports.getRestaurant = async (req, res, next) => {
     try {
         const restaurants = await restaurantModel.find({});
         res.status(200).json(restaurants);
-    } catch (err) {
-        next(err);
+    } catch (error) {
+        console.log(error)
     }
 };
 
+exports.getRestaurantById = async(req, res, next) => {
+    //restaurantModel.findById(req.params.id);
+    try {
+        const restaurants = await restaurantModel.findById({});
+        res.status(200).json(restaurants);
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+exports.updateRestaurant = async(req, res, next) => {
+    restaurantModel.findByIdAndUpdate(req.params.id, req.body,{
+        new: true,
+        useFindAndModify: false
+    })
+    try {
+        const restaurants = await restaurantModel.findByIdAndUpdate({});
+        res.status(200).json(restaurants);
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+exports.deleteRestaurant = async(req, res, next) => {
+    try {
+        const restaurants = await restaurantModel.findByIdAndDelete({});
+        res.status(200).json(restaurants);
+    } catch (error) {
+        console.log(error)
+    }
+};
